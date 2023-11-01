ec089661eba48eb919b5655eb050255fbb8d9ffe
Aluno Senai <G3hunterbugs@gmail.com>
2023-10-31 21:58:52 -0300

    atividade

--- Aula 31.10/Atividade 5/main.dart
+++ Aula 31.10/Atividade 5/main.dart
@@ -1,30 +1,13 @@
 void main () {
+int a = 0;
+int b = 1;
+ print(b);
+ for(int i = 2; i <=10; i++) {
 
-// A será a variavel de repetição
-  for(int a=1; a<=100; a++) {
-
-
-//B tem 1  e C é A+B / C = 2   
-int b=1;
-int c=a+b;
-
-if (a <= b && b <= c) {
-print(a);
-
+ int c = a+b;
+ print(c);
+ a=b;
+ b=c;
 }
-    
 
-if (b <= c && c <= a) {
-print(b);
-
-}
-    
-
-if (c <= a && a <= b) {
-print(c);
-
-}
-    
-
- }
 }
